4e744f9939426228a96596e7d3a3528071028f4d
Christian Manalansan <christian.manalansan@cgifederal.com>
2016-07-20 14:34:03 -0400

    Creating a Block (Plugin)
    Create a 'HugStatus' class at 'src/Plugin/Block'.
    Extend from BlockBase core class.
    Add @Block annotation.
    Add blockForm, blockSubmit and build methods.

---
+++ src/Plugin/Block/HugStatus.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\hugs\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'HugStatus' block.
+ *
+ * @Block(
+ *  id = "hug_status",
+ *  admin_label = @Translation("Hug status"),
+ * )
+ */
+class HugStatus extends BlockBase {
+
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+         'hugging_enabled' => $this->t(''),
+        ] + parent::defaultConfiguration();
+  
+ }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['hugging_enabled'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Hugging enabled'),
+      '#description' => $this->t('Check for Hugging enabled website'),
+      '#default_value' => $this->configuration['hugging_enabled'],
+      '#weight' => '0',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['hugging_enabled'] = $form_state->getValue('hugging_enabled');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['hug_status_hugging_enabled']['#markup'] = '<p>' . $this->configuration['hugging_enabled'] . '</p>';
+
+    return $build;
+  }
+
+}
